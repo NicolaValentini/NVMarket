a449eb3af40f45e74b703c00f7e78d5651fc6199
Nicola Valentini <nicolavalentini7@gmail.com>
2026-05-18 18:11:58 +0200

    feat: add product actions

---
+++ lib/actions/products.ts
@@ -0,0 +1,124 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+
+import { validateProduct } from '../utils';
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  getProductByName,
+  getProductsWithTags,
+  getProductWithTagsById,
+  updateProduct,
+} from '../db/api/products';
+
+export async function getProductsWithTagsAction() {
+  const result = getProductsWithTags();
+
+  if (result.isError) {
+    result.message = 'Something went wrong during products fetching';
+    return result;
+  }
+
+  return result;
+}
+
+export async function getProductByIdAction(id: string) {
+  const result = getProductById(id);
+
+  if (result.isError) {
+    result.message = 'Something went wrong during product fetching';
+    return result;
+  }
+
+  return result;
+}
+
+export async function getProductWithTagsByIdAction(id: string) {
+  const result = getProductWithTagsById(id);
+
+  if (result.isError) {
+    result.message = 'Something went wrong during product fetching';
+    return result;
+  }
+
+  return result;
+}
+
+export async function createProductAction(formData: FormData) {
+  const name = formData.get('name') as string;
+  const tags = (formData.get('tags') as string)?.split(',')?.filter(Boolean);
+
+  const errors = validateProduct(name, tags);
+
+  if (Object.keys(errors).length) return errors;
+
+  const duplicated = getProductByName(name);
+
+  if (duplicated.isError) {
+    errors.result = 'Something went wrong during validation';
+    return errors;
+  }
+
+  if (duplicated.data?.id) {
+    errors.name = 'Already exist a product with this name';
+    return errors;
+  }
+
+  const result = createProduct(name, tags);
+
+  if (result.isError) {
+    errors.result = 'Something went wrong during product creation';
+    return errors;
+  }
+
+  revalidatePath('/[locale]/products', 'page');
+
+  return {};
+}
+
+export async function updateProductAction(formData: FormData) {
+  const id = formData.get('id') as string;
+  const name = formData.get('name') as string;
+  const tags = (formData.get('tags') as string)?.split(',')?.filter(Boolean);
+  const errors = validateProduct(name, tags);
+
+  if (Object.keys(errors).length) return errors;
+
+  const duplicated = getProductByName(name);
+
+  if (duplicated.isError) {
+    errors.result = 'Something went wrong during validation';
+    return errors;
+  }
+
+  if (duplicated.data?.id && duplicated.data.id !== id) {
+    errors.name = 'Already exist a product with this name';
+    return errors;
+  }
+
+  const result = updateProduct(id, name, tags);
+
+  if (result.isError) {
+    errors.result = 'Something went wrong during product updating';
+    return errors;
+  }
+
+  revalidatePath('/[locale]/products', 'page');
+
+  return errors;
+}
+
+export async function deleteProductAction(id: string) {
+  const result = deleteProduct(id);
+
+  if (result.isError) {
+    result.message = 'Something went wrong during product deleting';
+    return result;
+  }
+
+  revalidatePath('/[locale]/products', 'page');
+
+  return result;
+}
