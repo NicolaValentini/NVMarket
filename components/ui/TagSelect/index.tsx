154de040d51f748834ce8f1da39812179fed6bbf
Nicola Valentini <nicolavalentini7@gmail.com>
2026-05-18 18:12:46 +0200

    feat: add TagSelect

---
+++ components/ui/TagSelect/index.tsx
@@ -0,0 +1,85 @@
+import { FC } from 'react';
+
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+import { Tag } from '@/lib';
+
+import { TagChip } from '../TagChip';
+
+type Props = {
+  tags: Tag[];
+  selectedTags: string[];
+  setTags: (tags: string[]) => void;
+  disabled?: boolean;
+  error?: string | undefined;
+};
+
+export const TagSelect: FC<Props> = ({
+  tags,
+  selectedTags,
+  setTags,
+  disabled,
+  error,
+}) => {
+  const handleChange = (event: SelectChangeEvent<string[]>) => {
+    const {
+      target: { value },
+    } = event;
+    setTags(typeof value === 'string' ? value.split(',') : value);
+  };
+
+  return (
+    <>
+      <FormControl fullWidth error={!!error} disabled={disabled}>
+        <InputLabel id='tags-label'>Tags</InputLabel>
+
+        <Select
+          multiple
+          value={selectedTags}
+          onChange={handleChange}
+          labelId='tags-label'
+          input={<OutlinedInput label='Tags' />}
+          renderValue={selected => (
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+              {selected.map(value => (
+                <TagChip key={value} tag={JSON.parse(value)} />
+              ))}
+            </Box>
+          )}
+        >
+          {tags.map(tag => {
+            const value = JSON.stringify(tag);
+            const selected = selectedTags.includes(value);
+
+            return (
+              <MenuItem key={tag.id} value={value}>
+                <Checkbox checked={selected} />
+
+                <TagChip tag={tag} />
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+
+      {error && (
+        <Typography variant='caption' color='error'>
+          {error}
+        </Typography>
+      )}
+
+      <input
+        type='hidden'
+        name='tags'
+        value={selectedTags.map(tag => JSON.parse(tag)?.id)}
+      />
+    </>
+  );
+};
--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,5 +2,7 @@ export * from './ColoredListItem';
 export * from './ColorInput';
 export * from './EmptyState';
 export * from './PageHeader';
+export * from './QuantitySelector';
 export * from './RouterDialog';
 export * from './TagChip';
+export * from './TagSelect';
